3014c26d75b6b94895737bc2c8af22fbfd8519ad
Sushrut1101 <guptasushrut@gmail.com>
2022-02-10 23:14:59 +0530

    f62: Import WiFi Configs
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -29,3 +29,10 @@ $(call inherit-product, vendor/samsung/f62/f62-vendor.mk)
 PRODUCT_PACKAGES += \
 	fstab.exynos9825 \
 	ueventd.exynos9825
+
+# WiFi
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/wifi/indoorchannel.info:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/indoorchannel.info \
+	$(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+	$(LOCAL_PATH)/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
+	$(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
