14b41f0fdfd5a2a2346a4cc7e2ae2779f74feb53
Sushrut1101 <guptasushrut@gmail.com>
2022-02-28 16:00:52 +0530

    f62: enable support for freeform windows and picture-in-picture
    Co-authored-by: Tim Zimmermann <tim@linux4.de>
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -121,6 +121,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
 
+# PIP
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.picture_in_picture.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.picture_in_picture.xml
+
 # Tethering
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
